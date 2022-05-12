4479ed2cc02d052ef85c367b14f264f341e26692
uabinesh <79247690+uabinesh@users.noreply.github.com>
2022-05-12 14:50:07 +0530

    logger added

---
+++ backend/src/utils/logger.js
@@ -0,0 +1,15 @@
+const pino = require("pino")
+
+const logger = pino({
+    transport:{
+        target:"pino-pretty",
+        options:{
+            colorize:true,
+            translateTime:`SYS:yyyy-mm-dd HH:MM:ss o`,
+            ignore:"pid,hostname",
+            sync:false
+        }
+    }
+});
+
+module.exports = logger
