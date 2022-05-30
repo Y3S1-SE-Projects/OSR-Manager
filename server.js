68b44a8da38aeb5adb674f9b6b5aecf8b4fdb40f
Chabbax <chandurdissanayake0@gmail.com>
2022-05-31 01:03:28 +0530

    my first commit

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
 const path = require("path");
-require("dotenv").config();
-
 const logger = require("./src/utils/logger");
 const config = require("./src/config/index");
 const DB_connect = require("./src/config/database.connection");
+require("dotenv").config();
 
 const app = express();
 const PORT = config.SOCKET_PORT || 4000;
@@ -15,14 +13,6 @@ const PORT = config.SOCKET_PORT || 4000;
 app.use(express.json());
 app.use(cors());
 app.use(bodyParser.json());
-
-mongoose
-  .connect(process.env.MONGODB_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log("MongoDB has been connected"))
-  .catch((err) => console.log(err));
 
 //routes
 app.use("/student", require("./src/api/routes/Student.route"));
@@ -33,15 +23,13 @@ app.use("/categories", require("./src/api/routes/Categories.route"));
 
 // middleware will import the client build folder to the server.
 app.use(express.static(path.join(__dirname, "/client/build")));
+
 // will ensure that the routes defined with React Router are working once the application has been deployed. It handles any requests by redirecting them to index.html
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "/client/build", "index.html"));
 });
 
-// app.listen(process.env.PORT || 4000, () => {
-//   console.log(`Server started on port ${process.env.PORT}`);
-// });
-
+// Server Port
 app.listen(process.env.PORT || 4000, () => {
   logger.info(`Server is up and running on port number: ${PORT}`);
   DB_connect().then(() => {
