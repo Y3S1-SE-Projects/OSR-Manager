5eac16dda766697328f89163bbdefe3a70e70cb8
Chabbax <chandurdissanayake0@gmail.com>
2022-05-30 19:16:08 +0530

    my first commit

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const path = require("path");
+
 require("dotenv").config();
 
 const logger = require("./src/utils/logger");
