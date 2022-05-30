80c2c3b4722bd0eadb1f6618c2b16059c422f4b1
Chabbax <chandurdissanayake0@gmail.com>
2022-05-31 00:31:37 +0530

    my first commit

--- server.js
+++ server.js
@@ -30,15 +30,12 @@ app.use("/users", require("./src/api/routes/User.route"));
 app.use("/posts", require("./src/api/routes/Post.route"));
 app.use("/categories", require("./src/api/routes/Categories.route"));
 
-// Accessing the path module
-const path = require("path");
-
 // middleware will import the client build folder to the server.
-app.use(express.static(path.resolve(__dirname, "client", "build")));
-
+app.use(express.static(path.join(__dirname, "client", "build")));
+const path = require("path");
 // will ensure that the routes defined with React Router are working once the application has been deployed. It handles any requests by redirecting them to index.html
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
 });
 
 app.listen(process.env.PORT || 4000, () => {
