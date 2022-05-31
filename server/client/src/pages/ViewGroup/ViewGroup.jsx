fa4c0cdcbdc1133ea45789df25703d1936c415d5
uabinesh <79247690+uabinesh@users.noreply.github.com>
2022-06-01 02:17:06 +0530

    View group added

---
+++ server/client/src/pages/ViewGroup/ViewGroup.jsx
@@ -0,0 +1,99 @@
+import * as React from 'react';
+import Link from '@mui/material/Link';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
+import CssBaseline from "@mui/material/CssBaseline";
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import TableContainer from '@mui/material/TableContainer';
+
+// Generate Order Data
+function createData(id, date, name, shipTo, paymentMethod, amount) {
+    return { id, date, name, shipTo, paymentMethod, amount };
+}
+
+const rows = [
+    createData(
+        0,
+        '16 Mar, 2019',
+        'Elvis Presley',
+        'Tupelo, MS',
+        'VISA ⠀•••• 3719',
+        312.44,
+    ),
+    createData(
+        1,
+        '16 Mar, 2019',
+        'Paul McCartney',
+        'London, UK',
+        'VISA ⠀•••• 2574',
+        866.99,
+    ),
+    createData(2, '16 Mar, 2019', 'Tom Scholz', 'Boston, MA', 'MC ⠀•••• 1253', 100.81),
+    createData(
+        3,
+        '16 Mar, 2019',
+        'Michael Jackson',
+        'Gary, IN',
+        'AMEX ⠀•••• 2000',
+        654.39,
+    ),
+    createData(
+        4,
+        '15 Mar, 2019',
+        'Bruce Springsteen',
+        'Long Branch, NJ',
+        'VISA ⠀•••• 5919',
+        212.79,
+    ),
+];
+
+function preventDefault(event) {
+    event.preventDefault();
+}
+const theme = createTheme();
+
+export default function Orders() {
+    return (
+
+        <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <Grid item xs={12}>
+                <Paper sx={{ p: 2, display: 'flex', flexDirection: 'column' }}>
+
+            <h2>Recent Orders</h2>
+            <Table size="small" sx={{ maxWidth: 950 }}  component={Paper} elevation={7}>
+                <TableHead>
+                    <TableRow>
+                        <TableCell>Date</TableCell>
+                        <TableCell>Name</TableCell>
+                        <TableCell>Ship To</TableCell>
+                        <TableCell>Payment Method</TableCell>
+                        <TableCell align="right">Sale Amount</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {rows.map((row) => (
+                        <TableRow key={row.id}>
+                            <TableCell>{row.date}</TableCell>
+                            <TableCell>{row.name}</TableCell>
+                            <TableCell>{row.shipTo}</TableCell>
+                            <TableCell>{row.paymentMethod}</TableCell>
+                            <TableCell align="right">{`$${row.amount}`}</TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+            <Link color="primary" href="#" onClick={preventDefault} sx={{ mt: 3 }}>
+                See more orders
+            </Link>
+                </Paper>
+            </Grid>
+
+        </ThemeProvider>
+    );
+}
