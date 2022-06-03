4b5633ce67288d0d3b370cd56d095af9194b4746
ZIHARA M.I.F <86686054+Zihara@users.noreply.github.com>
2022-06-03 23:54:14 +0530

    env variable changed

--- src/config/index.js
+++ src/config/index.js
@@ -3,6 +3,9 @@ const configs = {
     DB_CONNECTION:process.env.MONGODB_URL,
     SOCKET_PORT:process.env.PORT,
     SENDER_EMAIL:process.env.EMAIL_USER,
-    SENDER_PASSWORD:process.env.EMAIL_PWD
+    SENDER_PASSWORD:process.env.EMAIL_PWD,
+    CLOUD_NAME:process.env.CLOUD_NAME,
+    API_KEY:process.env.API_KEY,
+    API_SECRET:process.env.API_SECRET
 };
 module.exports = configs;
